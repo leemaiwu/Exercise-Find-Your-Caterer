64d4fa53248179cb36b24f29556752202adf2149
Laura Wu <leemaiwu@gmail.com>
2023-05-05 22:41:28 -0600

    Initial commit

---
+++ solution.js
@@ -0,0 +1,10 @@
+function findCaterer(budget, people){
+  
+  return 0;
+}
+
+console.log(findCaterer(940, 70)) // -1
+console.log(findCaterer(200, 9)) // 2
+console.log(findCaterer(150, 10)) // 1
+console.log(findCaterer(300, 9)) // 3
+console.log(findCaterer(100, 0)) // -1
