44efdd9d31d2b0e90dd6645120e01257768c86db
Laura Wu <leemaiwu@gmail.com>
2023-05-07 10:37:36 -0600

    Completed with ternary and if else statements

--- solution.js
+++ solution.js
@@ -1,6 +1,20 @@
 function findCaterer(budget, people){
-  
-  return 0;
+  let final = []
+  let first = people * 15
+  let second = people * 20
+  let third = people > 60 ? (people * 30) * 0.80 : people * 30
+  if (people === 0) {
+    return -1
+  } else if (budget > third) {
+    final.push(3)
+  } else if (budget > second) {
+    final.push(2)
+  } else if (budget >= first) {
+    final.push(1)
+  } else {
+    return -1
+  }
+  return +final
 }
 
 console.log(findCaterer(940, 70)) // -1
